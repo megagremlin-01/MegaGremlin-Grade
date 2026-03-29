f79f4dd3ad088c09c01bb7a677b333fe0c5c8653
MegaGremlin <megagremlin@megagremlin.com>
2026-03-29 17:45:55 -0400

    Update grading.hlsl

--- src/grading.hlsl
+++ src/grading.hlsl
@@ -1,7 +1,6 @@
-// MegaGremlin Grade - High Precision Linearization
+// OBS Shader Header
 uniform float4x4 ViewProj;
-uniform texture2d image;
-uniform float gain; // This links to the C++ slider
+uniform texture2d image; // OBS uses 'image' as the default input identifier
 
 sampler_state textureSampler {
     Filter    = Linear;
@@ -14,24 +13,33 @@ struct VertData {
     float2 uv  : TEXCOORD0;
 };
 
-// The Resolve Secret: Math must be done in Linear Space
-float3 toLinear(float3 sRGB) {
-    return pow(abs(sRGB), 2.2);
+struct PixelData {
+    float4 pos : OVERLAY_COORD; // OBS specific semantic
+    float2 uv  : TEXCOORD0;
+};
+
+PixelData VSDefault(VertData v_in)
+{
+    PixelData vert_out;
+    vert_out.pos = mul(float4(v_in.pos.xyz, 1.0), ViewProj);
+    vert_out.uv  = v_in.uv;
+    return vert_out;
 }
 
-float3 toSRGB(float3 linearRGB) {
-    return pow(abs(linearRGB), 1.0 / 2.2);
+float4 PSDrawLowLatency(PixelData p_in) : TARGET
+{
+    // Sample the current frame
+    float4 rgba = image.Sample(textureSampler, p_in.uv);
+    
+    // This is where we will insert the Oklab math tomorrow
+    return rgba; 
 }
 
-float4 mainTransform(VertData v_in) : TARGET {
-    float4 rgba = image.Sample(textureSampler, v_in.uv);
-    
-    // 1. Convert to 32-bit Linear
-    float3 color = toLinear(rgba.rgb);
-    
-    // 2. Apply Gain (Our first real-time test)
-    color *= gain; 
-    
-    // 3. Convert back for OBS display
-    return float4(toSRGB(color), rgba.a);
+technique Draw
+{
+    pass
+    {
+        vertex_shader = VSDefault(v_in);
+        pixel_shader  = PSDrawLowLatency(p_in);
+    }
 }
